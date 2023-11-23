b3e05f0fc73c28ebc8fe05b96edd9999fa45360b
ArchanaChau <100211814+ArchanaChau@users.noreply.github.com>
2023-11-23 22:29:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 
 import express from "express";
+import cors from "cors";
 import userRoutes from "./userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use([express.urlencoded({ extended: true }), express.json()]);
 
 app.use("/api", userRoutes);
